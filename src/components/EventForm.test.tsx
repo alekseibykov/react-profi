ef27b020176bbf719424ddc0c51025f0bcae4802
Aleksei Bykov <gogohandz@gmail.com>
2025-05-03 21:20:38 +0300

    add tests

--- src/components/EventForm.test.tsx
+++ src/components/EventForm.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import EventForm from './EventForm';
-import { IUser } from '../models/IUser';
 
 // Mock dependencies
 jest.mock('../utils/rules', () => ({
